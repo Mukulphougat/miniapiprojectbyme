7e25882fa7a8da9a651274d7a37dbaf94024f8db
Mukul <mukulphoughat@gmail.com>
2022-07-18 15:18:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import './cardStyle.css';
+import {useState} from "react";
+import axios from "axios";
+import Card from "./Card";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [changed, setChanged] = useState(false);
+  const [key, setKey] = useState(0);
+  const [newData, setNewData] = useState({});
+
+  async function getReq() {
+    const res = await axios.get("https://randomuser.me/api/?inc=gender,name,nat,location,picture,email&results=%2020");
+    setData(res.data.results)
+  }
+  function setChange(curr,index){
+    console.log(index)
+    console.log(curr)
+    setChanged(true)
+    setKey(index)
+    setNewData(curr)
+  }
+  return (
+      <div className="App">
+        <div className={"navbar"}>
+          <div className={"navbar-item"}>
+            <div className={"product"}>Product</div>
+            <div>Download</div>
+            <div>Pricing</div>
+          </div>
+          {/*<input type={"text"} placeholder={"Enter Name"} onChange={(e) => setItem(e.target.value)}/>*/}
+        </div>
+        {/*<h1>{item}</h1>*/}
+        <button onClick={() => getReq()} className={"buttons"}>Get</button>
+        <div>
+          {
+            changed ?
+                <Card data={newData} key={key}/>
+                :
+                <h1>Click On Get Button..</h1>
+          }
+        </div>
+        <div className={"containers"}>
+          {
+
+            // eslint-disable-next-line array-callback-return
+            data.map((curr, index) => {
+              return (
+                  <div className={"card-container"} onClick={() => setChange(curr,index)} key={index}>
+                    <div>{curr.gender}.{curr.nat}</div>
+                    <div>{curr.name.title+" "+curr.name.first+" "+curr.name.last}</div>
+                    <div>{curr.email}</div>
+                  </div>
+              )
+            })
+          }
+
+        </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import "./cardStyle.css"
+export default function Card(props){
+    return(
+        <div className={"card-container1"}>
+            <img className={"profile"} width={100} height={100} src={props.data.picture.large} alt={"profile"}/>
+            <div className={"info"}>
+                <div className={"name"}>{props.data.name.title+" "+props.data.name.first+" "+props.data.name.last}</div>
+                <div className={"details"}>{props.data.location.street.number+" "+props.data.location.street.name+" "+props.data.location.city+" "+props.data.location.state+" "+props.data.location.country+" "+props.data.location.postcode}</div>
+                <div className={"details"}>{props.data.location.timezone.offset+" "+props.data.location.timezone.description}</div>
+            </div>
+        </div>
+    )
+}
